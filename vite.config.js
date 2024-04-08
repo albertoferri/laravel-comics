b0b503a2ef0914f85e5858370f0acc4cd6689a71
albertoferri <falberto99@gmail.com>
2024-04-08 17:35:24 +0200

    aggiunte immagini e creato file db.php

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
-const path = require('path');
+import path from 'path';
 
 export default defineConfig({
     plugins: [
